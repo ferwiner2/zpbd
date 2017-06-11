3b7bd944b0e1f55e75c0c40a6cef36753f9600fd
rkluz <rkluz.pl@gmail.com>
2017-06-11 12:11:18 +0200

    pomiar czasu operacji na bazie

--- src/main/java/pl/pw/edu/elka/zpbd/wikiReader/Main.java
+++ src/main/java/pl/pw/edu/elka/zpbd/wikiReader/Main.java
@@ -1,23 +1,49 @@
 package pl.pw.edu.elka.zpbd.wikiReader;
 
+import pl.pw.edu.elka.zpbd.databases.SearchResult;
+import pl.pw.edu.elka.zpbd.databases.cassandra.CassandraSearchEngine;
+import pl.pw.edu.elka.zpbd.wikiReader.documentCreator.CassandraDocCreator;
 import pl.pw.edu.elka.zpbd.wikiReader.documentCreator.DocumentCreator;
-import pl.pw.edu.elka.zpbd.wikiReader.documentCreator.RedisDocCreator;
+//import pl.pw.edu.elka.zpbd.wikiReader.documentCreator.RedisDocCreator;
 import pl.pw.edu.elka.zpbd.wikiReader.freader.FileReader;
 import pl.pw.edu.elka.zpbd.wikiReader.freader.Runner;
+
+import java.util.List;
 
 public class Main {
 
 
     public static void main(String[] args) throws Exception {
-        DocumentCreator dp = new RedisDocCreator();
+//        DocumentCreator dp = new CassandraDocCreator();
+//
+//        dp.init();
+//        Runner p = new Runner(dp, FileReader.Mode.PL);
+//
+//        p.runLoop();
+//
+//        dp.close();
+//        dp.timer.showTime();
+        String[] arguments = {"War%", "Wa%", "W%", "%war%", "%wa%", "%w%","A%","li%","%li%","ele%", "%ele%", "do%" ,"%do%","d%","%d%"};
+        CassandraSearchEngine cse = new CassandraSearchEngine();
+        try {
+            for (int i = 0; i < arguments.length; ++i) {
 
-        dp.init();
-        Runner p = new Runner(dp, FileReader.Mode.PL);
+                long startTime = System.currentTimeMillis();
+                List<SearchResult> sr = cse.search("select * from wiki_pages.pl where title like \'" + arguments[i] + "\';");
+                long endTime = System.currentTimeMillis();
+                long time = (endTime - startTime);
+                System.out.print(arguments[i]);
+                System.out.print(" ");
+                System.out.print(sr.size());
+                System.out.print(" ");
+                System.out.println(time);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
-        p.runLoop();
 
-        dp.close();
-        dp.timer.showTime();
-
+        cse.getCassandraDb().close();
     }
 }
